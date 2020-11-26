650af5a3580dd8a9a79c353428148f1ea7e338ed
fh_bots <69167693+fh-bots@users.noreply.github.com>
2020-11-26 22:23:26 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -7,12 +7,12 @@ class Translation(object):
     CH_LIST = """<b>empty</b>"""  
     CAP_TION = """
  ✪༺ ──•◈•─ ─•◈•──༻✪
- ᴄʜᴀɴɴᴇʟ ➧ @MM_MalluMovies
- ᴄʜᴀɴɴᴇʟ ➧ @MM_TamilMovies
- ᴄʜᴀɴɴᴇʟ ➧ @MalluMoviesALL
- ᴄʜᴀɴɴᴇʟ ➧ @Mallu_Rockers
- ᴄʜᴀɴɴᴇʟ ➧ @MM_TVSeries
- ᴄʜᴀɴɴᴇʟ ➧ @MM_NEW
- ᴄʜᴀɴɴᴇʟ ➧ @MM_OLD
- ᴄʜᴀɴɴᴇʟ ➧ @MM_Linkz
+ ᴄʜᴀɴɴᴇʟ ➧ @filimhouse
+ ᴄʜᴀɴɴᴇʟ ➧ @hit_tv_series
+ ᴄʜᴀɴɴᴇʟ ➧ @fh_webseries
+ ᴄʜᴀɴɴᴇʟ ➧ @fh_dubbed
+ ᴄʜᴀɴɴᴇʟ ➧ @fh_south
+ ᴄʜᴀɴɴᴇʟ ➧ @fh_links
+ ᴄʜᴀɴɴᴇʟ ➧ @fh_stream
+ ᴄʜᴀɴɴᴇʟ ➧ @fh_hevc
 🎗️ʝσιи 🎗️ ѕнαяє🎗️ ѕυρρσят🎗️"""
